1988767b8c897ae22a933ae8681a31be0d6d4f55
Alex Good <AGood@rightpoint.com>
2019-06-02 19:11:12 -0500

    Initial check in

---
+++ server/swaggerton-server/Controllers/GenerateValueController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace swaggerton_server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class GenerateValueController : ControllerBase
+    {
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id,string value1, string value2)
+        {  
+            return "value_" + id.ToString() + value1 + "_" + value2;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
